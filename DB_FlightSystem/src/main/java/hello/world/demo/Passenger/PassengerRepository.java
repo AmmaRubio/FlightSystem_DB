3c0c22bee6e94de18e1e894362ea4007439dfc0f
AmmaRubio <104092614+AmmaRubio@users.noreply.github.com>
2022-07-27 10:03:14 +0200

    find Flights by Passenger Id

--- DB_FlightSystem/src/main/java/hello/world/demo/Passenger/PassengerRepository.java
+++ DB_FlightSystem/src/main/java/hello/world/demo/Passenger/PassengerRepository.java
@@ -1,9 +1,14 @@
 package hello.world.demo.Passenger;
 
+import hello.world.demo.Flight.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PassengerRepository extends JpaRepository<Passenger, Long> {
 
+    Optional<Flight> findFlightById(long id);
 }
